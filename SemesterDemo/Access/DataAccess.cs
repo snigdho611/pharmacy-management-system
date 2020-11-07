4039b680b1b1afaa534f454f753e655a986a8e1d
Snigdho Dip Howlader <61914319+snigdho611@users.noreply.github.com>
2020-11-08 00:28:04 +0600

    Created Data Access class

--- SemesterDemo/Access/DataAccess.cs
+++ SemesterDemo/Access/DataAccess.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,28 @@ namespace SemesterDemo.Access
 {
     class DataAccess
     {
+        private String ConnectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=D:\\Snigdho\\Github\\Pharmacy_Management_System\\SemesterDemo\\DB_SAF.mdf;Integrated Security=True;Connect Timeout=30";
+
+        private SqlConnection sqlCon;
+        private SqlCommand sqlCom;
+        private SqlDataAdapter sqlDataAdapter;
+
+        public SqlConnection SqlCon
+        {
+            set { this.sqlCon = value; }
+            get { return this.sqlCon; }
+        }
+
+        public SqlCommand SqlCom
+        {
+            set { this.sqlCom = value; }
+            get { return this.sqlCom; }
+        }
+
+        public SqlDataAdapter SqlDataAdapter
+        {
+            set { this.sqlDataAdapter = value; }
+            get { return this.sqlDataAdapter; }
+        }
     }
 }
